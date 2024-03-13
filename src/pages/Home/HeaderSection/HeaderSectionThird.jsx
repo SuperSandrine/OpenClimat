ce12a2dbd2b1910d20ca62638ef4b893af33a860
Sandrine Mestas Gleizes <98587061+SuperSandrine@users.noreply.github.com>
2024-03-13 13:00:32 +0100

    animation on img setled

--- src/pages/Home/HeaderSection/HeaderSectionThird.jsx
+++ src/pages/Home/HeaderSection/HeaderSectionThird.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import imageSVG from '../../../assets/OC_SCREEN_DESKTOP_ANIM1-01 1.svg'
+import imageSVGBig from '../../../assets/ANIM2.svg'
 import arrow from '../../../assets/Vector.svg'
 import ButtonCTA from '../../../components/Buttons/ButtonCTA'
 
@@ -7,31 +8,35 @@ import ButtonCTA from '../../../components/Buttons/ButtonCTA'
 const HeaderSectionThird = () => {
   
   return (
-    <div className="flex justify-evenly pt-[5.5rem] bg-gradient-to-b from-bg-dark-blue via-bg-middle-blue to-bg-light-pink h-screen overflow-hidden">
+    <div className="flex justify-evenly pt-[7rem] bg-gradient-to-b from-bg-dark-blue via-bg-middle-blue to-bg-light-pink h-screen overflow-hidden">
       
-        <div className="flex flex-col w-[23.4375rem] pt-[15rem] container-text-fadeOut">
+        <div className="flex flex-col w-[23.4375rem] pt-[8rem] container-fadeOut">
 
-          <section className='text-fadeOut  min-h-[51rem]' id="introFirst3">
+          <section className=' min-h-[51rem]' id="introFirst3">
             <h2 className="text-5xl text-white " >
               Rejoignez<br/> la plateforme ouverte<br/> de l’action climatique</h2>
           </section>
 
-          <section className='text-fadeOut  min-h-[51rem] opacity-0'id="introSecond3">
+          <section className=' min-h-[51rem] opacity-0'id="introSecond3">
             <h2 className="text-5xl text-white" >Une page pédagogique, fiable et comparable pour communiquer et progresser avec tout votre écosystème.</h2>
           </section>
 
-          <section className="text-fadeOut  min-h-[31rem] text-5xl text-white opacity-1" id="introThird3">
+          <section className=" min-h-[31rem] text-5xl text-white opacity-1" id="introThird3">
             <h2 className='pb-[8rem]'>N'agissez plus seul. Rejoignez le mouvement et réussissons ensemble le défi de notre siècle.</h2>
             <ButtonCTA>Rejoindre le mouvement <img src={arrow}/></ButtonCTA>
           </section>
 
         </div>
 
-        <div className='pt-[15rem]
-          '>
+        <div className='pt-[8rem] container-fadeOut relative'>
 
-          <img src={imageSVG} className=" w-[32.94rem] object-top object-contain" />
+          <img src={imageSVG} className=" w-[32.94rem] object-top object-contain" id="introImgFirst3" />
+
+          <img src={imageSVGBig} className=" w-[32.94rem] opacity-0 absolute origin-left pl" id="introImgSecond3"
+          />
         </div>
+
+
       </div>
 
 
