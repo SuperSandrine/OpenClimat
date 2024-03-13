5962908d534faa61622a123b13eade108ba6dda9
Sandrine Mestas Gleizes <98587061+SuperSandrine@users.noreply.github.com>
2024-03-13 11:31:24 +0100

    animation on texts settled

--- src/components/Buttons/ButtonCTA.jsx
+++ src/components/Buttons/ButtonCTA.jsx
@@ -3,7 +3,7 @@ import Arrow from '../SmallElements/Arrow'
 
 const ButtonCTA = ({children}) => {
   return (
-    <button className="text-bg-dark-blue text-[1rem] rounded-[4px] bg-white flex items-center gap-2 pr-[1.5rem] pl-[2.5rem] py-[0.9rem] ">
+    <button className="text-bg-dark-blue text-[1rem] rounded-[4px] bg-white flex items-center gap-2 pr-[1.5rem] pl-[2.5rem] py-[0.9rem] group ">
       {children}
       <Arrow fill="#080e3e"/>
     </button>
